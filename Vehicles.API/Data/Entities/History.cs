7ae28b6d4041f6cc6f7ac1f654a32b0951a0d570
Lucas Giraldo <lucasgcorrales@gmail.com>
2021-09-05 20:33:24 -0500

    Complete crud in users

--- Vehicles.API/Data/Entities/History.cs
+++ Vehicles.API/Data/Entities/History.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Vehicles.API.Data.Entities
@@ -18,9 +19,9 @@ namespace Vehicles.API.Data.Entities
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}")]
         public DateTime Date { get; set; }
 
-        //[Display(Name = "Fecha")]
-        //[DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}")]
-        //public DateTime DateLocal => Date.ToLocalTime();
+        [Display(Name = "Fecha")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}")]
+        public DateTime DateLocal => Date.ToLocalTime();
 
         [Display(Name = "Kilometraje")]
         [DisplayFormat(DataFormatString = "{0:N0}")]
@@ -30,5 +31,26 @@ namespace Vehicles.API.Data.Entities
         [DataType(DataType.MultilineText)]
         public string Remarks { get; set; }
 
+        [JsonIgnore]
+        [Display(Name = "Mecánico")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public User User { get; set; }
+
+        public ICollection<Detail> Details { get; set; }
+
+        [Display(Name = "# Detalles")]
+        public int DetailsCount => Details == null ? 0 : Details.Count;
+
+        [Display(Name = "Total Mano de Obra")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal TotalLabor => Details == null ? 0 : Details.Sum(x => x.LaborPrice);
+
+        [Display(Name = "Total Repuestos")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal TotalSpareParts => Details == null ? 0 : Details.Sum(x => x.SparePartsPrice);
+
+        [Display(Name = "Total")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal Total => Details == null ? 0 : Details.Sum(x => x.TotalPrice);
     }
 }
--- Vehicles.API/Models/UserViewModel.cs
+++ Vehicles.API/Models/UserViewModel.cs
@@ -58,6 +58,6 @@ namespace Vehicles.API.Models
         [Display(Name = "Foto")]
         public string ImageFullPath => ImageId == Guid.Empty
             ? $"https://localhost:44345/images/noimage.png"
-            : $"https://vehicleszulu.blob.core.windows.net/users/{ImageId}";
+            : $"https://vehicleslg.blob.core.windows.net/users/{ImageId}";
     }
 }
